f580ca915e787a59605ed151948d710246403071
Md. Rejwan Kabir Hamim <107980219+RejwankabirHamim@users.noreply.github.com>
2023-04-30 12:33:55 +0600

    Add files via upload

---
+++ Math/FFT/2.cpp
@@ -0,0 +1,121 @@
+//https://www.spoj.com/problems/ADAMATCH/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using    namespace __gnu_pbds;
+using    namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define dbg(x) cout<<#x<<": "<<x<<endl;
+#define M  998244353 // 1000000007  //
+#define ll long long
+#define pa pair<ll,ll>
+#define ff first
+#define ss second
+#define pb push_back
+#define pi acos(-1.0)
+#define vi vector<int>
+#define vll vector<ll>
+#define fr(i,n,j) for(i=j;i<=n;i++)
+#define rfr(i,n,j) for(i=n;i>=j;i--)
+#define ct continue;
+#define yo cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define all(v) v.begin(),v.end()
+#define endl '\n'
+#define mx 1000000
+ll ar[500005],br[500005],cr[500005],dr[500005];
+vector<int>v[100005];
+const int N = 3e5 + 9;
+
+const double PI = acos(-1);
+struct base {
+  double a, b;
+  base(double a = 0, double b = 0) : a(a), b(b) {}
+  const base operator + (const base &c) const
+    { return base(a + c.a, b + c.b); }
+  const base operator - (const base &c) const
+    { return base(a - c.a, b - c.b); }
+  const base operator * (const base &c) const
+    { return base(a * c.a - b * c.b, a * c.b + b * c.a); }
+};
+void fft(vector<base> &p, bool inv = 0) {
+  int n = p.size(), i = 0;
+  for(int j = 1; j < n - 1; ++j) {
+    for(int k = n >> 1; k > (i ^= k); k >>= 1);
+    if(j < i) swap(p[i], p[j]);
+  }
+  for(int l = 1, m; (m = l << 1) <= n; l <<= 1) {
+    double ang = 2 * PI / m;
+    base wn = base(cos(ang), (inv ? 1. : -1.) * sin(ang)), w;
+    for(int i = 0, j, k; i < n; i += m) {
+      for(w = base(1, 0), j = i, k = i + l; j < k; ++j, w = w * wn) {
+        base t = w * p[j + l];
+        p[j + l] = p[j] - t;
+        p[j] = p[j] + t;
+      }
+    }
+  }
+  if(inv) for(int i = 0; i < n; ++i) p[i].a /= n, p[i].b /= n;
+}
+vector<long long> multiply(vector<int> &a, vector<int> &b) {
+  int n = a.size(), m = b.size(), t = n + m - 1, sz = 1;
+  while(sz < t) sz <<= 1;
+  vector<base> x(sz), y(sz), z(sz);
+  for(int i = 0 ; i < sz; ++i) {
+    x[i] = i < (int)a.size() ? base(a[i], 0) : base(0, 0);
+    y[i] = i < (int)b.size() ? base(b[i], 0) : base(0, 0);
+  }
+  fft(x), fft(y);
+  for(int i = 0; i < sz; ++i) z[i] = x[i] * y[i];
+  fft(z, 1);
+  vector<long long> ret(sz);
+  for(int i = 0; i < sz; ++i) ret[i] = (long long) round(z[i].a);
+ // while((int)ret.size() > 1 && ret.back() == 0) ret.pop_back();
+  return ret;
+}
+int main()
+{
+    fast;
+    ll te,n,m,i,j,k,w1,x,y,z,l2,k2,k1,q,l,r;
+    te=1;
+    //cin>>te;
+    fr(w1,te,1)
+    {
+        string s,p;
+        cin>>s>>p;
+        reverse(all(p));
+        n=s.size();
+        m=p.size();
+        map<char,int>mp;
+        mp['C']=1,mp['T']=2;mp['G']=3;
+        vector<ll>ans[4];
+        fr(k,3,0)
+        {
+            vector<int>a,b;
+            fr(i,n-1,0)
+            {
+                if(mp[s[i]]==k)a.pb(1);
+                else a.pb(0);
+            }
+            fr(i,m-1,0)
+            {
+                if(mp[p[i]]==k)b.pb(1);
+                else b.pb(0);
+            }
+            ans[k]=multiply(a,b);
+
+        }
+        z=0;
+        rfr(i,n-m,0)
+        {
+            y=i+m-1;
+            x=0;
+            fr(k,3,0)x+=ans[k][y];
+            //dbg(x);
+            z=max(z,x);
+        }
+        //dbg(z);
+        cout<<m-z<<endl;
+    }
+}
