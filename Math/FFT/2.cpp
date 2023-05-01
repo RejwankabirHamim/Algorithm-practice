a54f546c7ba66126ec4841e9155e7f820ea60359
Md. Rejwan Kabir Hamim <107980219+RejwankabirHamim@users.noreply.github.com>
2023-05-01 12:15:15 +0600

    Add files via upload

---
+++ Math/FFT/2.cpp
@@ -0,0 +1,151 @@
+//https://codeforces.com/contest/754/problem/E
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using    namespace __gnu_pbds;
+using    namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define dbg(x) cout<<#x<<": "<<x<<endl;
+#define M  998244353 // 1000000007  //
+#define ll long long
+#define pa pair<ll,ll>
+#define ff first
+#define ss second
+#define pb push_back
+#define pi acos(-1.0)
+#define vi vector<int>
+#define vll vector<ll>
+#define fr(i,n,j) for(i=j;i<=n;i++)
+#define rfr(i,n,j) for(i=n;i>=j;i--)
+#define ct continue;
+#define yo cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define all(v) v.begin(),v.end()
+#define endl '\n'
+#define mx 1000000
+ll ar[500005],br[500005],cr[500005],dr[500005];
+vector<int>v[100005];
+const int N = 3e5 + 9;
+
+const double PI = acos(-1);
+struct base {
+  double a, b;
+  base(double a = 0, double b = 0) : a(a), b(b) {}
+  const base operator + (const base &c) const
+    { return base(a + c.a, b + c.b); }
+  const base operator - (const base &c) const
+    { return base(a - c.a, b - c.b); }
+  const base operator * (const base &c) const
+    { return base(a * c.a - b * c.b, a * c.b + b * c.a); }
+};
+void fft(vector<base> &p, bool inv = 0) {
+  int n = p.size(), i = 0;
+  for(int j = 1; j < n - 1; ++j) {
+    for(int k = n >> 1; k > (i ^= k); k >>= 1);
+    if(j < i) swap(p[i], p[j]);
+  }
+  for(int l = 1, m; (m = l << 1) <= n; l <<= 1) {
+    double ang = 2 * PI / m;
+    base wn = base(cos(ang), (inv ? 1. : -1.) * sin(ang)), w;
+    for(int i = 0, j, k; i < n; i += m) {
+      for(w = base(1, 0), j = i, k = i + l; j < k; ++j, w = w * wn) {
+        base t = w * p[j + l];
+        p[j + l] = p[j] - t;
+        p[j] = p[j] + t;
+      }
+    }
+  }
+  if(inv) for(int i = 0; i < n; ++i) p[i].a /= n, p[i].b /= n;
+}
+vector<base> multiply(vector<base> &a, vector<base> &b) {
+  int n = a.size(), m = b.size(), t = n + m - 1, sz = 1;
+  while(sz < t) sz <<= 1;
+  vector<base> x(sz), y(sz), z(sz);
+  for(int i = 0 ; i < sz; ++i) {
+    x[i] = i < (int)a.size() ? a[i] : base(0, 0);
+    y[i] = i < (int)b.size() ? b[i] : base(0, 0);
+  }
+  fft(x), fft(y);
+  for(int i = 0; i < sz; ++i) z[i] = x[i] * y[i];
+  fft(z, 1);
+  vector<base> ret(sz);
+  for(int i = 0; i < sz; ++i) ret[i] =z[i];// base(round(z[i].a),round(z[i].b));
+ // while((int)ret.size() > 1 && ret.back() == 0) ret.pop_back();
+  return ret;
+}
+int main()
+{
+    fast;
+    ll te,n,m,i,j,k,w1,x,y,z,l2,k2,k1,q,l,r;
+    te=1;
+    //cin>>te;
+    fr(w1,te,1)
+    {
+        ll c;
+        cin>>n>>m;
+        string str[805],pat[405];
+        fr(i,n-1,0)cin>>str[i];
+        cin>>l>>c;
+        fr(i,n-1,0)
+        {
+            fr(j,m+c-2,m)str[i]+=str[i][j%m];
+        }
+        fr(i,n+l-2,n)str[i]=str[i%n];
+        z=0;
+        rfr(i,l-1,0)
+        {
+            cin>>pat[i];
+            fr(j,c-1,0)z+=(pat[i][j]=='?');
+            fr(j,m+c-2,c)pat[i]+='?';
+            reverse(all(pat[i]));
+        }
+        z=l*c-z;
+        vector<base>a,b,ans;
+        double alpa,d1,d2;
+        fr(i,n+l-2,0)
+        {
+            fr(j,m+c-2,0)
+            {
+                alpa=(2*pi*(str[i][j]-'a'))/26.0;
+                a.pb({cos(alpa),sin(alpa)});
+            }
+        }
+        fr(i,l-1,0)
+        {
+            x=i*(m+c-1);
+            fr(j,m+c-2,0)
+            {
+                if(pat[i][j]=='?')
+                {
+                    b.pb({0,0});
+                    ct;
+                }
+                alpa=(2*pi*(pat[i][j]-'a'))/26.0;
+                b.pb({cos(alpa),sin(-alpa)});
+            }
+        }
+        ans=multiply(a,b);
+      //  fr(i,ans.size()-1,0)cout<<ans[i].a<<" "<<ans[i].b<<endl;
+        k=l*(m+c-1)-1;
+        //dbg(z);
+        fr(i,n-1,0)
+        {
+            x=i*(m+c-1);
+            fr(j,m-1,0)
+            {
+                y=x+j+k;
+               // dbg(y);
+              // cout<<ans[y].a<<" "<<ans[y].b<<endl;
+               //dbg(ans[y].a);
+                if(abs(ans[y].b)<1e-3&&abs(z-ans[y].a)<1e-3)
+                {
+                    cout<<1;
+                }
+                else cout<<0;
+            }
+            cout<<endl;
+        }
+
+    }
+}
