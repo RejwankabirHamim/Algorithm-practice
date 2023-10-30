42467e292c4b00ee55ae9475aa0d3c8b0323c18b
Md. Rejwan Kabir Hamim <107980219+RejwankabirHamim@users.noreply.github.com>
2023-10-30 14:01:02 +0600

    Create Counting Patterns.cpp

---
+++ String/Suffix Automaton/Counting Patterns.cpp
@@ -0,0 +1,98 @@
+//https://cses.fi/problemset/task/2103/
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using    namespace __gnu_pbds;
+using    namespace std;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define dbg(x) cout<<#x<<": "<<x<<endl;
+#define M   998244353 //1000000007  //
+#define ll long long
+#define pa pair<ll,ll>
+#define ff first
+#define ss second
+#define pb push_back
+#define pi acos(-1.0)
+#define vi vector<int>
+#define vll vector<ll>
+#define fr(i,n,j) for(i=j;i<=n;i++)
+#define rfr(i,n,j) for(i=n;i>=j;i--)
+#define ct continue;
+#define yo cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define all(v) v.begin(),v.end()
+#define endl '\n'
+struct state {
+    int len, link;
+    bool iscloned;
+    map<char, int> next;
+};
+const int MAXLEN = 100000;
+state st[MAXLEN * 2];
+int sz, last;
+void sa_init() {
+    st[0].len = 0;
+    st[0].link = -1;
+    for(int i=0;i<MAXLEN*2;i++)st[i].iscloned=0;
+    sz++;
+    last = 0;
+}
+void sa_extend(char c) {
+    int cur = sz++;
+    st[cur].len = st[last].len + 1;
+    int p = last;
+    while (p != -1 && !st[p].next.count(c)) {
+        st[p].next[c] = cur;
+        p = st[p].link;
+    }
+    if (p == -1) {
+        st[cur].link = 0;
+    } else {
+        int q = st[p].next[c];
+        if (st[p].len + 1 == st[q].len) {
+            st[cur].link = q;
+        } else {
+            int clone = sz++;
+            st[clone].len = st[p].len + 1;
+            st[clone].next = st[q].next;
+            st[clone].link = st[q].link;
+            st[clone].iscloned=1;
+            while (p != -1 && st[p].next[c] == q) {
+                st[p].next[c] = clone;
+                p = st[p].link;
+            }
+            st[q].link = st[cur].link = clone;
+        }
+    }
+    last = cur;
+}
+int tot[200003];
+int main()
+{
+    fast
+    ll i,j,k,l,x,y,n,m;
+    string s,p;
+    cin>>s;
+    vector<pa>v;
+    sa_init();
+    n=s.size();
+    fr(i,n-1,0)sa_extend(s[i]);
+    fr(i,sz-1,0)tot[i]=(!st[i].iscloned),v.pb({st[i].len,i});
+    sort(all(v),greater<pa>());
+    fr(i,sz-2,0)tot[st[v[i].ss].link]+=tot[v[i].ss];
+    cin>>k;
+    while(k--)
+    {
+        cin>>p;
+        m=p.size();
+        x=0;
+        fr(i,m-1,0)
+        {
+            if(st[x].next[p[i]])x=st[x].next[p[i]];
+            else break;
+        }
+        if(i==m)cout<<tot[x]<<endl;
+        else cout<<0<<endl;
+    }
+}
