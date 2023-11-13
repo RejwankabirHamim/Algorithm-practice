234fdf3ebb910fe7aa4d060bf4a3a45493ab58a0
Md. Rejwan Kabir Hamim <107980219+RejwankabirHamim@users.noreply.github.com>
2023-11-13 23:20:35 +0600

    Update Kings in Chessboard.cpp

--- Math/Matrix Exponenciation/Kings in Chessboard.cpp
+++ Math/Matrix Exponenciation/Kings in Chessboard.cpp
@@ -4,63 +4,6 @@ using namespace std;
 #define int  long long
 #define pb push_back
 const int mod = 4294967296;
- int mod_inverse( int x, int p)//x is fact[i] and p is M-2
-{
-    int res=1,y=p;
-    while(y>0)
-    {
-        if(y&1) res=((res%mod)*(x%mod))%mod;
-        y=y>>1;
-        x=((x%mod)*(x%mod))%mod;
-    }
-    return res;
-}
-int power(long long n, long long k) {
-  int ans = 1 % mod; n %= mod; if (n < 0) n += mod;
-  while (k) {
-    if (k & 1) ans = (long long) ans * n % mod;
-    n = (long long) n * n % mod;
-    k >>= 1;
-  }
-  return ans;
-}
-int Gauss(vector<vector<int>> a, vector<int> &ans){
-  int n = a.size(), m = (int)a[0].size() - 1;
-  vector <int> pos(m, -1);
-  int free_var = 0;
-  const long long MODSQ = (long long)mod * mod;
-  int det = 1, rank = 0;
-  for (int col = 0, row = 0; col < m && row < n; col++) {
-    int mx = row;
-    for (int k = row; k < n; k++) if (a[k][col] > a[mx][col]) mx = k;
-    if (a[mx][col] == 0) {det = 0; continue;}
-    for (int j = col; j <= m; j++) swap(a[mx][j], a[row][j]);
-    if (row != mx) det = det == 0 ? 0 : mod - det;
-    det = 1LL * det * a[row][col] % mod;
-    pos[col] = row;
-    int inv = power(a[row][col], mod - 2);
-    for (int i = 0; i < n && inv; i++){
-      if (i != row && a[i][col]) {
-        int x = ((long long)a[i][col] * inv) % mod;
-        for (int j = col; j <= m && x; j++){
-          if (a[row][j]) a[i][j] = (MODSQ + a[i][j] - ((long long)a[row][j] * x)) % mod;
-        }
-      }
-    }
-    row++; ++rank;
-  }
-  ans.assign(m, 0);
-  for (int i = 0; i < m; i++){
-    if (pos[i] == -1) free_var++;
-    else ans[i] = ((long long)a[pos[i]][m] * power(a[pos[i]][i], mod - 2)) % mod;
-  }
-  for (int i = 0; i < n; i++) {
-    long long val = 0;
-    for (int j = 0; j < m; j++) val = (val + ((long long)ans[j] * a[i][j])) % mod;
-    if (val != a[i][m]) return -1; //no solution
-  }
-  return free_var; //has solution
-}
 struct Mat {
   int n, m;
   vector<vector<int>> a;
